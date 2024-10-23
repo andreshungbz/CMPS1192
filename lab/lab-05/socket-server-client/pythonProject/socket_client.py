d8d1ff71a83adce0e404e9f54425ab55d19c5144
Andres Hung <andres.hung@outlook.com>
2024-10-23 15:57:32 -0600

    Make general improvements

--- lab/lab-05/socket-server-client/pythonProject/socket_client.py
+++ lab/lab-05/socket-server-client/pythonProject/socket_client.py
@@ -1,54 +1,52 @@
 import socket
+import argparse
 
-def client_program():
+def client_program(ip, port):
     # Program Introduction
-
-    print("-- Simple Socket Client Messaging Program --")
-    print("Ensure the server IP address is configured correctly\n")
+    print("-- Socket Messaging Client --")
 
     # Client Information
-
-    print("-- Client Information --")
-
-    client_hostname = socket.gethostname() # computer hostname
-    print(f"Client Hostname: {client_hostname}")
-
-    ip_addresses = socket.gethostbyname_ex(socket.gethostname())[-1] # list of computer ip addresses
-    print(f"Available Client IP Addresses: {ip_addresses}")
-
-    client_ip = next((ip for ip in ip_addresses if ip.startswith('10.0')), None) # choose ip starting with 10.0
+    client_hostname = socket.gethostname()  # client hostname
+    ip_addresses = socket.gethostbyname_ex(client_hostname)[-1]  # list of client ip addresses
+    client_ip = next((ip for ip in ip_addresses if ip.startswith('10.0')), None)  # choose ip starting with 10.0
     if not client_ip:
-        raise ValueError("No IP address starting with '10.' found.") # error checking
-    print(f"Chosen Client IP Address: {client_ip}\n")
+        raise ValueError("No IP address starting with '10.' found.")  # error checking
 
-    # Configure Server to Contact
-
-    server_ip = '10.0.57.166' # set server ip to contact
-    port = 6000 # set port
-    max_bytes = 2048  # set max bytes of packet
+    print("\n[Client Information]")
+    print(f"Client Hostname: {client_hostname}")
+    print(f"Client IP Addresses: {ip_addresses}")
 
     # Client Socket Configuration
+    server_ip = ip # set server ip that client will contact
+    server_port = port # set server port to that client will connect to
+    max_bytes = 2048  # set max bytes of packet
 
     client_socket = socket.socket() # create socket
-    client_socket.connect((server_ip, port)) # connect to server
+    client_socket.connect((server_ip, server_port)) # connect to server
+
+    print("\n[Client Configuration]")
+    print(f"Client with IP Address {client_ip} set to contact Server with IP Address {server_ip} on Port {server_port}\n")
 
     # Initial Connection
-
     # send client host name to server as first message
     client_socket.send(client_hostname.encode())  # send hostname to server
 
-    # Connection Loop
-
+    # Client Loop
     message = input(" -> ") # accept input
     while message.lower().strip() != 'exit': # exit loop when the input is 'exit'
         client_socket.send(message.encode()) # send message to server
-        data = client_socket.recv(max_bytes).decode() # receive server response
 
-        print(data) # show in client terminal
+        data = client_socket.recv(max_bytes).decode() # receive server response
+        print(data) # show server message in client terminal
 
         message = input(" -> ") # accept another input
 
-    client_socket.close() # close connection
+    # Closedown
+    client_socket.close() # close client socket
 
 if __name__ == '__main__':
-    client_program()
+    parser = argparse.ArgumentParser(description='Socket Messaging Client')
+    parser.add_argument('ip', type=str, help='Server IP address to connect to')
+    parser.add_argument('port', type=int, help='Port number to connect to')
+    args = parser.parse_args()
+    client_program(args.ip, args.port)
--- lab/lab-05/socket-server-client/pythonProject/socket_server.py
+++ lab/lab-05/socket-server-client/pythonProject/socket_server.py
@@ -1,62 +1,61 @@
 import socket
+import argparse
 
-def server_program():
+def server_program(port):
     # Program Introduction
-
-    print("-- Simple Socket Server Messaging Program --")
-    print("Ensure the socket client program is configured with the chosen server IP address\n")
+    print("-- Socket Messaging Server --")
 
     # Server Information
-
-    print("-- Server Information --")
-
-    server_hostname = socket.gethostname() # computer hostname
-    print(f"Server Hostname: {server_hostname}")
-
-    ip_addresses = socket.gethostbyname_ex(socket.gethostname())[-1] # list of computer ip addresses
-    print(f"Available Server IP Addresses: {ip_addresses}")
-
-    server_ip = next((ip for ip in ip_addresses if ip.startswith('10.0')), None) # choose ip starting with 10.0
+    server_hostname = socket.gethostname()  # server hostname
+    ip_addresses = socket.gethostbyname_ex(server_hostname)[-1]  # list of server ip addresses
+    server_ip = next((ip for ip in ip_addresses if ip.startswith('10.0')), None)  # choose ip starting with 10.0
     if not server_ip:
-        raise ValueError("No IP address starting with '10.' found.") # error checking
-    print(f"Chosen Server IP Address: {server_ip}")
+        raise ValueError("No IP address starting with '10.' found.")  # error checking
+
+    print("\n[Server Information]")
+    print(f"Server Hostname: {server_hostname}")
+    print(f"Server IP Addresses: {ip_addresses}")
 
     # Server Socket Configuration
-
-    port = 6000 # set port
+    server_port = port # set server port that will listen
     max_bytes = 2048 # set max bytes of packet
+
     server_socket = socket.socket() # create socket
-    server_socket.bind((server_ip, port)) # bind to socket the ip and port
+    server_socket.bind((server_ip, server_port)) # bind to socket the ip and port
     server_socket.listen(4) # set max number of simultaneous clients
 
-    print("\nWaiting...\n")
+    print("\n[Server Configuration]")
+    print(f"Server listening on socket bound to IP Address {server_ip} and Port {server_port}")
 
     # Initial Connection
-
-    connection, address = server_socket.accept() # accept new connection
-
+    print("\nWaiting...\n")
+    client, address = server_socket.accept() # accept new connection
     # store the client's hostname, which is its first message to the server
-    client_hostname = connection.recv(max_bytes).decode()
-    print(f"Connection from {str(address[0])} (hostname: {client_hostname})\n")
+    client_hostname = client.recv(max_bytes).decode()
+    print(f"{client_hostname} connected with IP Address {str(address[0])} from Port {str(address[1])})\n")
 
-    # Connection Loop
-
+    # Server Loop
     while True:
-        data = connection.recv(max_bytes).decode() # receive data stream
+        data = client.recv(max_bytes).decode() # receive data stream
 
         # exit when client closes the connection
-        if not data:
+        if not data: # data being 0 bytes does this
             break
 
-        # show client message on server terminal
+        # show client message in server terminal
         print(client_hostname + ": " + str(data))
 
         # write a message from the server and send to client
         data = server_hostname + ": "
         data += input(" -> ")
-        connection.send(data.encode())
+        client.send(data.encode()) # convert string data into bytes object
 
-    connection.close() # close connection
+    # Closedown
+    client.close() # close client connection
+    server_socket.close() # close server socket
 
 if __name__ == '__main__':
-    server_program()
+    parser = argparse.ArgumentParser(description='Socket Messaging Server')
+    parser.add_argument('port', type=int, help='Port number to bind the server')
+    args = parser.parse_args()
+    server_program(args.port)
